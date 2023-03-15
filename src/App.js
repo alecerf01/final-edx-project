1b4e7095cdfaa5ea7da937ea1b3dc12b13593c80
Berserk <alexlecerf0131@gmail.com>
2023-03-15 19:36:23 +0000

    removed some starter code to try a different project idea

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-import 'firebase/auth'
 
-import { useAuthState } from 'react-firebase-hooks/auth'
-import { useCollectionData } from 'react-firebase-hooks/firestore'
-
-firebase.initializeApp({
-
-})
-
-const auth = firebase.auth();
-const firestore = firebase.firestore();
 
 function App() {
   return (
